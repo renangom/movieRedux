64b792f36eab5df852afde58f232efc31435e2a5
Renan <renan23eduardo@gmail.com>
2022-09-18 20:41:42 -0300

    Create header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./Pages/Home";
 import { MovieDetail } from "./Pages/MovieDetail/MovieDetail";
+import './App.scss'
 function App() {
 
   return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,16 @@
-
+import { Link } from "react-router-dom"
+import user from '../../images/user.png'
+import './header.scss'
 
 export const Header = () => {
     return (
-        <h1> This is header </h1>
+        <div className="header">
+            <Link to='/'>
+                <div className="logo">Movie App</div>
+            </Link>
+            <div className="user-image">
+                <img src={user} alt="user" />
+            </div>
+        </div>
     )
 }
